74b8d25a709a168b01259f15b888d86064dbefd2
MariiaGalitsyna <111524740+MariiaGalitsyna@users.noreply.github.com>
2023-07-10 16:09:44 +0200

    test: test poll CMS

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
   public static void main(String[]args){
-    System.out.println("------***** Test poll CMS ****-----");
+    System.out.println("------***** Test poll CMS in EngIT Jenkins ****-----");
  }
 } 
