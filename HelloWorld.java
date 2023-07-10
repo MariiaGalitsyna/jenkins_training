9712315aaae888866c644bd07e7c62d15909679c
MariiaGalitsyna <111524740+MariiaGalitsyna@users.noreply.github.com>
2023-07-10 16:02:13 +0200

    test: test poll CMS

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
   public static void main(String[]args){
-    System.out.println("------***** Test webhook ****-----");
+    System.out.println("------***** Test poll CMS ****-----");
  }
 } 
